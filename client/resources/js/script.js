cf8d746e55919e04e3d91de48d41e195cb1e5f73
DinorexBG <kerzo.gm@gmail.com>
2021-03-12 21:02:47 +0200

    Renamed a function in scripts.js

--- client/resources/js/script.js
+++ client/resources/js/script.js
@@ -2,30 +2,28 @@ let isNavSticky = false;
 let mouseOver = false;
 
 window.onscroll = function () {
-    myFunction();
+    stickyNav();
 };
 
 var css = "nav-btns a:first-child:hover,.nav-btns a:first-child:active { background: #85c215; }";
 
-// Get the navbar
 var navbar = document.querySelector("nav");
 var loginBtn = document.querySelector(".nav-btns a:first-child");
 
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
 loginBtn.addEventListener("mouseover", function (event) {
     mouseOver = true;
     loginBtn.style.background = "#85c215";
 });
+
 loginBtn.addEventListener("mouseout", function (event) {
     mouseOver = false;
     if (isNavSticky) loginBtn.style.background = "#7ab214";
     else loginBtn.style.background = "none";
 });
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyNav() {
     if (window.pageYOffset > sticky) {
         isNavSticky = true;
         navbar.classList.add("sticky");
