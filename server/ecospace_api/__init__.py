146f23338c838bd87d2d70ebb57b110b3aee430a
bvpav <bvpav@tutanota.com>
2021-03-12 14:35:30 +0200

    Setup database and added User model

--- server/ecospace_api/__init__.py
+++ server/ecospace_api/__init__.py
@@ -1,11 +1,25 @@
+import os
+from contextlib import suppress
+
 from flask import Flask, jsonify
 from flask_restful import Api
 __version__ = '0.0.1'
-# __author__ = 'FalsePositive'
+__author__ = 'FalsePositive'
 
 def create_app():
     """Create and configure an instance of a Flask app"""
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+            # TODO: Configure linter to not complaina about long lines
+            SQLALCHEMY_DATABASE_URI=f'sqlite://{os.path.join(app.instance_path, "ecospace.sqlite")}',  # noqa
+    )
+
+    with suppress(OSError):
+        # Create the `scifedium/server/instance` folder where the db is
+        os.makedirs(app.instance_path)
+
+    from .models import db
+    db.init_app(app)
 
     @app.route('/flask_test')
     def hello():
---
+++ server/ecospace_api/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    # TODO: Add docstring
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(36), unique=True)
+    handle = db.Column(db.String(15), unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __str__(self):
+        return f'<User: @{self.handle} ({self.public_id})>'
+
