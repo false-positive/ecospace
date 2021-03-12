9900c88b944fe920582a0bc711e48cfa10efae08
bvpav <bvpav@tutanota.com>
2021-03-12 17:11:15 +0200

    Made the User and UserList resources query the db

--- server/ecospace_api/api/user.py
+++ server/ecospace_api/api/user.py
@@ -1,50 +1,44 @@
+import functools
+
 from flask_restful import abort, Resource
 
-users = {
-    'test': {
-        'password': 'rozovi_rozi',
-        'full_name': 'Joe',
-        'organizations_ids': ['6a2ddadc-8327-11eb-8dcd-0242ac130003'],
-    },
-    'test1': {
-        'password': 'rozovi_rozi',
-        'full_name': 'Bob',
-        'organizations_ids': ['6a2ddadc-8327-11eb-8dcd-0242ac130003'],
-    },
-    'test2': {
-        'password': 'rozovi_rozi',
-        'full_name': 'Ben',
-        'organizations_ids': ['6a2ddadc-8327-11eb-8dcd-0242ac130003'],
-    },
-}
+from ..models import User as UserModel  # TODO: Maybe rename the models to prevent collisions like this
+
+
+def pass_user(view):
+    """Decorator to pass a user a a parameter to a view"""
+
+    @functools.wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if 'username' in kwargs:
+            username = kwargs.pop('username')
+            user = UserModel.query.filter_by(username=username).first()
+            if not user:
+                abort(404, message=f'user with username {username} doesnt exist')
+            kwargs['user'] = user
+        return view(*args, **kwargs)
+
+    return wrapped_view
 
 
 class UserList(Resource):
     def get(self):
-        result = []
-        for name in users:
-            result.append({
-                'username': name,
-                'full_name': users[name]['full_name'],
-                'organizations_ids': users[name]['organizations_ids'],
-            })
+        users = UserModel.query.all()
         return {
-            'data': result,
+            'data': bool(users),  # TODO: Serialize to response json
             'message': 'users listed successfully',
         }
 
 
 class User(Resource):
-    def get(self, username):
-        if username not in users:
-            abort(404, message=f'user with username {username} doesnt exist')
-        user = users[username]
+    @pass_user
+    def get(self, user):
         return {
-                'data': {
-                    'username': username,
-                    'full_name': user['full_name'],
-                    'organizations_ids': user['organizations_ids'],
+            'data': {
+                'username': user.name,  # XXX: will crash unless Kaloyan fixes
+                'full_name': user.full_name,
+                'organizations_ids': None,  # user['organizations_ids'],
 
-                },
-                'message': 'user found successfully',
+            },
+            'message': 'user found successfully',
         }
