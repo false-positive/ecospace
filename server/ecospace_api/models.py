b98d580b07a9ee5807682e76a6693e25fd6462eb
bvpav <bvpav@tutanota.com>
2021-03-12 21:47:48 +0200

    Implemented Even model

--- server/ecospace_api/models.py
+++ server/ecospace_api/models.py
@@ -2,11 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# events = db.Table(
-#     'events',
-#     db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True),  # noqa
-#     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)   # noqa
-# )
+events = db.Table(
+    'events',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True),
+)
 
 
 class User(db.Model):
@@ -16,10 +16,10 @@ class User(db.Model):
     full_name = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80), nullable=False)
     # https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/#many-to-many-relationships
-    # events = db.relationship(
-    #     'Event', secondary=events,
-    #     lazy='subquery', backref=db.backref('users', lazy=True),
-    # )
+    events = db.relationship(
+        'Event', secondary=events,
+        lazy='subquery', backref=db.backref('users', lazy=True),
+    )
 
     def get_response(self):
         return {
@@ -32,12 +32,13 @@ class User(db.Model):
         return f'<User: @{self.handle} ({self.public_id})>'
 
 
-# class Event(db.Model):
-#     # TODO: Add docstring
-#     id = db.Column(db.Integer, primary_key=True)
-#     public_id = db.Column(db.String(36), unique=True)
-#     name = db.Column(db.String(50), nullable=False)
+class Event(db.Model):
+    # TODO: Add docstring
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(36), unique=True)
+    name = db.Column(db.String(50), nullable=False)
+    participants = db.relationship('User', secondary=events)
 
-#     def __str__(self):
-#         return f'<Event: {self.id}>'
+    def __str__(self):
+        return f'<Event: {self.id}>'
 
