583cc62bf0f7dcdf1fe4cecb757fa7d440211550
bvpav <bvpav@tutanota.com>
2021-03-13 18:33:06 +0200

    Added participants

--- server/ecospace_api/models.py
+++ server/ecospace_api/models.py
@@ -4,6 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+events = db.Table(
+    'events',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
+
 
 class UserModel(db.Model):
     """Database model for a registered user."""
@@ -15,6 +21,8 @@ class UserModel(db.Model):
     description = db.Column(db.String(250), default='', nullable=False)
     password = db.Column(db.String(80), nullable=False)
     organized_events = db.relationship('EventModel', backref='organizer', lazy=True)
+    # https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/#many-to-many-relationships
+    events = db.relationship('EventModel', secondary=events, lazy='subquery', backref=db.backref('participants', lazy=True))
 
     def get_response(self):
         return {
@@ -43,7 +51,7 @@ class EventModel(db.Model):
             'name': self.name,
             'date': self.date.isoformat(),
             'location': self.location,
-            'organizer': self.organizer_id,
+            'organizer_username': self.organizer.username,
         }
 
     def __str__(self):
