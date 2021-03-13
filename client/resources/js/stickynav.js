5df4c3ff001c1db06421b896c006084a1cdf1073
DinorexBG <kerzo.gm@gmail.com>
2021-03-13 11:39:19 +0200

    Fixed sticky navigation bug in home page

--- client/resources/js/stickynav.js
+++ client/resources/js/stickynav.js
@@ -3,16 +3,17 @@ window.onscroll = function () {
 };
 
 var navbar = document.querySelector("nav");
+var header = document.querySelector("header");
+
+header.style.height = navbar.style.height;
 
 var sticky = navbar.offsetTop;
 
 function stickyNav() {
     if (window.pageYOffset > sticky) {
-        navbar.classList.add("sticky");
         navbar.style.background = "#fff";
         navbar.style.borderBottom = "1px solid black";
     } else {
-        navbar.classList.remove("sticky");
         navbar.style.background = "none";
         navbar.style.borderBottom = "1px solid transparent";
     }
