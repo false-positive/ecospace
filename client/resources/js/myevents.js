456364adc430566bdfecc7ec6c9ed55e5f28be2f
bvpav <bvpav@tutanota.com>
2021-03-14 06:57:34 +0200

    Made creating an event redirect you to myevents

---
+++ client/resources/js/myevents.js
@@ -0,0 +1,38 @@
+let section = document.querySelector("section");
+
+let token = document.cookie.split(";")[0].split("=")[1];
+let { username } = jwt_decode(token);
+
+getUserInfo(`/${username}`).then(({ organized_events: response}) => {
+    for (const [id, event] of Object.entries(response)) {
+        let name = DOMPurify.sanitize(event.name);
+        console.log(name);
+        let location = DOMPurify.sanitize(event.location);
+        let date = new Date(DOMPurify.sanitize(event.date));
+
+        date = date.getFullYear() + "." + date.getMonth() + "." + date.getDate();
+
+        console.log(event);
+
+        let eventBoxHtml = `
+            <div class="row">
+                <div class="event">
+                    <div class="clearfix first-part">
+                        <h4>${name}</h4>
+                        <a href="moreinfo.html">More info</a>
+                    </div>
+                    <div class="clearfix second-part">
+                        <h5>Location: ${location}</h5>
+                    </div>
+                    <div class="third-part clearfix">
+                        <h5>Date: ${date}</h5>
+                        <a href="#" class="coming-btn coming" onclick='changeBtn()'>I'm coming :)</a>
+                    </div>
+                </div>
+            </div>
+            `;
+
+        section.insertAdjacentHTML("afterbegin", eventBoxHtml);
+    }
+});
+
