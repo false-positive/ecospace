9d3c2713ce7a37f461f332f075f268b0f8794726
bvpav <bvpav@tutanota.com>
2021-04-03 22:36:28 +0300

    Made page not refresh

--- ecospace/static/js/router.js
+++ ecospace/static/js/router.js
@@ -1,6 +1,6 @@
-const router = () => {
+const router = async () => {
     const routes = [
-        { path: "/events", view: "events" },
+        { path: "/events", view: EventListView },
         { path: "/events/new", view: "events/new" },
         { path: "/events/going", view: "events/going" },
         { path: "/events/my", view: "events/my" },
@@ -12,7 +12,28 @@ const router = () => {
     const match = potentialMatches.find(({ result }) => !!result);
     // TODO: handle no match
 
-    document.querySelector("#content").innerHTML = match.route.view;
+    const view = new match.route.view();
+
+    document.querySelector("#content").innerHTML = await view.getHTML();
 };
 
-document.addEventListener("DOMContentLoaded", router);
+// add the current page to the history and go to next page
+const navigateTo = (url) => {
+    window.history.pushState(null, null, url);
+    router();
+};
+
+// make the back and forward buttons work without reloading page
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    router();
+
+    // make all links with data-link attribute not refresh page
+    document.addEventListener("click", (e) => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+});
