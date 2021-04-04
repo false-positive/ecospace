9ac25f787c51498559ee4f187197020c8aeaa260
bvpav <bvpav@tutanota.com>
2021-04-04 22:06:39 +0300

    Moved login and register from JS to Flask

--- ecospace/auth.py
+++ ecospace/auth.py
@@ -3,8 +3,12 @@ The views for login and register.
 """
 
 import functools
+import datetime as dt
 
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .models import UserModel, db
 
 bp = Blueprint('auth', __name__)
 
@@ -16,18 +20,67 @@ def login_required(view):
     def wrapped_view(**kwargs):
         # XXX: If invalid token is present in cookie, it will still pass through
         # TODO: Validate token here.
-        if not request.cookies.get('token'):
+        if 'token' not in request.cookies:
+            flash('Please Log In to continue', category='message')
             return redirect(url_for('auth.login', then=request.path))
         return view(**kwargs)
 
     return wrapped_view
 
 
-@bp.route('/login')
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = UserModel.query.filter_by(username=username).first()
+
+        if not user or not check_password_hash(user.password, password):
+            flash('Invalid username or password')
+        else:
+            route = request.form.get('then') or 'events'
+            response = make_response(redirect(url_for('singlepage.index', route=route)))
+            response.set_cookie('token', user.encode_auth_token(), expires=dt.datetime.now() + dt.timedelta(days=30), samesite='strict')
+            return response
+
     return render_template('auth/login.html')
 
 
-@bp.route('/register')
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        print(request.form)
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if not first_name:
+            error = 'First Name is required'
+        elif not last_name:
+            error = 'Second Name is required'
+        elif not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+        elif UserModel.query.filter_by(username=username).first():
+            error = f'Username {username} is already taken'
+
+        if error:
+            flash(error)
+        else:
+            user = UserModel(
+                username=username,
+                password=generate_password_hash(password),
+                full_name=f'{first_name.strip()} {last_name.strip()}',
+            )
+            db.session.add(user)
+            db.session.commit()
+
+            route = request.form.get('then') or 'events'
+            response = make_response(redirect(url_for('singlepage.index', route=route)))
+            response.set_cookie('token', user.encode_auth_token(), expires=dt.datetime.now() + dt.timedelta(days=30), samesite='strict')
+            return response
+
     return render_template('auth/register.html')
