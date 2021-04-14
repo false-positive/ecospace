97b12871835a409ad5d9ec4fe1b1faa9e781c7ed
bvpav <bvpav@tutanota.com>
2021-04-14 21:26:31 +0300

    Made the my events page say no events

--- ecospace/static/js/views/MyEventsView.js
+++ ecospace/static/js/views/MyEventsView.js
@@ -6,15 +6,13 @@ class MyEventsView extends AbstractView {
 
     async getHTML() {
         const currentUser = await getUserInfo(currentUserUsername);
-        return `
-            <section class="section-nearme section-my-events">
-                <div class="row create-event-btn">
-                    <a href="/events/new" data-link>Create an event!</a>
-                </div>
-                ${Object.entries(currentUser.organized_events)
-                    .sort((a, b) => new Date(a[1].date) - new Date(b[1].date))
+        const events = Object.entries(currentUser.organized_events).sort((a, b) => new Date(a[1].date) - new Date(b[1].date));
+        let eventHTML;
+        if (events.length !== 0) {
+            eventHTML = `
+                ${events
                     .map(
-                        ([id, { name, location, date, participants }]) => `
+                        ([id, { name, location, date }]) => `
                         <div class="row">
                             <div class="event">
                                 <div class="clearfix first-part">
@@ -42,6 +40,29 @@ class MyEventsView extends AbstractView {
                         `
                     )
                     .join("")}
+            `;
+        } else {
+            eventHTML = `
+		    	<section class="section-info empty-page">
+				<div class="row">
+					<h3>You have no events</h3>
+				</div>
+				<div class="row">
+					<p>
+                        You haven't created any events yet. 
+                        <a href="/events/new" data-link>Create new event</a>
+                    </p>
+				</div>
+			</section>
+            `;
+        }
+
+        return `
+            <section class="section-nearme section-my-events">
+                <div class="row create-event-btn">
+                    <a href="/events/new" data-link>Create an event!</a>
+                </div>
+                ${eventHTML}
             </section>
         `;
     }
